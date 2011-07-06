37a2a625a9fe33c911fdf4b0f32b2085dd19ed4c
Denis <denis.labaye@gmail.com>
2011-07-06 22:41:14 +0200

    prettified a bit (find-unused)

--- src/jar_slimmer/core.clj
+++ src/jar_slimmer/core.clj
@@ -1,13 +1,11 @@
 (ns jar-slimmer.core)
 
-;; TODO
 (defn sorted-set? "true if input is a sorted set, false otherwise"
   [s] (and (set? s) (sorted? s)))
 
 (defn compl
   "A set which is the complement of minus to all"
   [all minus]
-  ;; FIXME this is a horrible pre condition,replace by sorted +set
   {:pre [(sorted-set? all) (sorted-set? minus)]}
   (apply sorted-set (remove #(minus %) all)))
 
@@ -29,31 +27,19 @@
   "Return the 1st half of the given seq"
   [s] (half s second))
 
-;; FIXME : optimize 
 ;; WARN: non-TCO optimized recursion, will blow the stack for deep trees !
 (defn find-unused
   "Find all elements of the set s for which (f (- s unused)) is true."
   ([s f] (apply sorted-set (find-unused s s f)))
-  ([all seg f] (if (seq seg)
-                 (if (next seg)
-                   (if (true-without? all seg f)
-                     seg
-                     (concat (find-unused all (first-half seg) f)
-                             (find-unused all (second-half seg) f)))
-                   (if (true-without? all seg f)
-                      seg
-                     (sorted-set)))
-                 (sorted-set))))
+  ([all seg f] (cond (empty? seg)      (sorted-set)
+                     (nil? (next seg)) (if (true-without? all seg f)
+                                         seg
+                                         (sorted-set))
+                     :otherwise        (if (true-without? all seg f)
+                                         seg
+                                         (concat (find-unused all (first-half seg) f)
+                                                 (find-unused all (second-half seg) f))))))
 
 (defn smallest
   "Return the smallest sub seq of the given seq for which the given function return true"
-  ([s f] (smallest s s f))
-  ([all current f] (let [fh (first-half current)
-                         sh (second-half current)]
-                     (if (seq current)
-                       (concat (if (true-without? all fh f)
-                                 fh
-                                 (smallest fh))
-                               (if (true-without? all sh f)
-                                 sh
-                                 (smallest sh)))))))
+  [s f] (compl s (find-unused s f)))
--- test/jar_slimmer/test/core.clj
+++ test/jar_slimmer/test/core.clj
@@ -36,7 +36,6 @@
       (at-least-in-2-4-or-6-8 [2 3 4 6 7 8 9]) => true
       (at-least-in-2-4-or-6-8 [2 3 4 6 7 9])   => false)
 
-;.;. Without work, all life goes rotten. -- Camus
 (fact (find-unused (sorted-set 2 3 4 6 7 8) at-least-in-2-4-or-6-8) => (sorted-set)
       (find-unused (sorted-set 1 2 3 4 5 6 7 8 9 10) at-least-in-2-4-or-6-8) => (sorted-set 1 5 9 10))
 
@@ -51,21 +50,11 @@
  (find-unused (sorted-set 1 2) contains-at-least-2?) => (sorted-set 1)
  (find-unused (sorted-set 1 2 3) contains-at-least-2?) => (sorted-set 1 3))
 
-;; FIXME
-'(fact (smallest [] pred) => []
-      (provided (pred []) => true))
 
-;; FIXME
-'(fact (smallest ["a"] (fn [x] true)) => ["a"])
-
-;; FIXME
-'(fact (smallest ["a" "b"] pred) => ["a" "b"]
-      (provided (pred ["a" "b"]) => true))
-
-;; FIXME
-'(fact (smallest ["a" "b"] pred) => ["b"]
-      (provided (pred ["a"]) => false
-                (pred ["b"]) => true))
-
-
+(let [ab (sorted-set :a :b)
+      a (sorted-set :a)
+      b (sorted-set :b)]
+  (fact (smallest ab pred) => a
+        (provided (find-unused ab (exactly pred)) => b
+                  (compl ab b) => a)))
 
